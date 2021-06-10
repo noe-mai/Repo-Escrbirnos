15cc80c67e9e6c39843f4c2885ff9f2b9351942c
noe-mai <noelia.s.maiquez@gmail.com>
2021-06-10 00:14:16 -0300

    Agregue normalize, y cambios en footer

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-/* primero guardo en constantes los elementos HTML que necesito */
+/* Responsive con JS */
+
 const burgerMenuBtn = document.querySelector('#burger-menu-toggler');
-/* En este caso, al ser varios items y seleccionarlos todos, lo que se guarda
-en la constante menuItems es una coleccion de nodos*/
+
+
 const menuItems = document.querySelectorAll('.menu-item');
 
 
 function navResponsive() {
 
-    /* Agrego o quito una clase al body */
+  
     burgerMenuBtn.addEventListener( 'click', function() {
         document.body.classList.toggle('mobile-menu-active');
     });
 
 
-    /* Al clickear un item del menu mobile, este se cierra */
+ 
      menuItems.forEach(function(e) {
       
         e.addEventListener('click', function() {
@@ -21,7 +22,6 @@ function navResponsive() {
         })
     });
 
-     /* agrego la clase active para dar estilos a los items del menu clickeados */
      for(let i = 0; i < menuItems.length; i++){
         menuItems[i].addEventListener('click', function () {
             let currentItem = document.querySelector('.active');
